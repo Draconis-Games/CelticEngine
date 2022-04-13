69210afef670c6b166c484c452a892eaf79ac793
Maxim Savenkov <max_savenkov@mail.ru>
2022-04-14 00:48:15 +0200

    Matrix4f refactor

--- src/main/java/com/draconisgames/celticengine/physics/math/matrices/Matrix4f.java
+++ src/main/java/com/draconisgames/celticengine/physics/math/matrices/Matrix4f.java
@@ -6,7 +6,7 @@ import static java.lang.Math.sqrt;
 
 public class Matrix4f {
 
-    public float[][] matrix = new float[4][4];
+    protected float[][] matrix = new float[4][4];
 
     public Matrix4f() {
         for (int i = 0; i < 4; i++) {
@@ -89,107 +89,11 @@ public class Matrix4f {
         return s.substring(0, s.length() - 3);
     }
 
-    public static Matrix4f fromQuaternion(Quaternion q) {
-
-        float x2 = q.getX() * q.getX();
-        float xy = q.getX() * q.getY();
-        float xz = q.getX() * q.getZ();
-        float xw = q.getX() * q.getW();
-        float y2 = q.getY() * q.getY();
-        float yz = q.getY() * q.getZ();
-        float yw = q.getY() * q.getW();
-        float z2 = q.getZ() * q.getZ();
-        float zw = q.getZ() * q.getW();
-
-        float[][] m = new float[4][4];
-
-        m[0][0] = 1 - 2 * (y2 + z2);
-        m[0][1] = 2 * (xy - zw);
-        m[0][2] = 2 * (xz + yw);
-
-        m[1][0] = 2 * (xy + zw);
-        m[1][1] = 1 - 2 * (x2 + z2);
-        m[1][2] = 2 * (yz - xw);
-
-        m[2][0] = 2 * (xz - yw);
-        m[2][1] = 2 * (yz + xw);
-        m[2][2] = 1 - 2 * (x2 + y2);
-
-        m[3][3] = 1;
-
-        return new Matrix4f(m);
+    public float[][] getMatrix() {
+        return matrix;
     }
 
-    public Quaternion toQuaternion() {
-        Quaternion q = new Quaternion();
-        q.setW((float) (sqrt(1.0 + matrix[0][0] + matrix[1][1] + matrix[2][2]) / 2.0));
-	    float w4 = (float) (4.0 * q.getW());
-	    q.setX((matrix[2][1] - matrix[1][2]) / w4);
-	    q.setY((matrix[0][2] - matrix[2][0]) / w4);
-	    q.setZ((matrix[1][0] - matrix[0][1]) / w4);
-        return q;
+    public void setMatrix(float[][] matrix) {
+        this.matrix = matrix;
     }
-
-    public static Matrix4f fromScale(Vector3 scale) {
-        Matrix4f m = new Matrix4f();
-        m.matrix[0][0] = scale.getX();
-        m.matrix[1][1] = scale.getY();
-        m.matrix[2][2] = scale.getZ();
-        return m;
-    }
-
-    public Vector3 toScale() {
-        Vector3 scale = new Vector3();
-        scale.setX(matrix[0][0]);
-        scale.setY(matrix[1][1]);
-        scale.setZ(matrix[2][2]);
-        return scale;
-    }
-
-    public static Matrix4f fromPosition(Vector3 pos) {
-        Matrix4f m = new Matrix4f();
-        m.matrix[0][3] = pos.getX();
-        m.matrix[1][3] = pos.getY();
-        m.matrix[2][3] = pos.getZ();
-        return m;
-    }
-
-    public Vector3 toPosition() {
-        return new Vector3(matrix[0][3], matrix[1][3], matrix[2][3]);
-    }
-
-    public static Matrix4f fromTransform(Transform t) {
-        float[][] m = new float[4][4];
-
-        float x2 = t.getRotation().getX() * t.getRotation().getX();
-        float xy = t.getRotation().getX() * t.getRotation().getY();
-        float xz = t.getRotation().getX() * t.getRotation().getZ();
-        float xw = t.getRotation().getX() * t.getRotation().getW();
-        float y2 = t.getRotation().getY() * t.getRotation().getY();
-        float yz = t.getRotation().getY() * t.getRotation().getZ();
-        float yw = t.getRotation().getY() * t.getRotation().getW();
-        float z2 = t.getRotation().getZ() * t.getRotation().getZ();
-        float zw = t.getRotation().getZ() * t.getRotation().getW();
-
-        m[0][0] = (1 - 2 * (y2 + z2)) * t.getScale().getX();
-        m[0][1] = 2 * (xy - zw) * t.getScale().getY();
-        m[0][2] = 2 * (xz + yw) * t.getScale().getZ();
-
-        m[1][0] = 2 * (xy + zw) * t.getScale().getX();
-        m[1][1] = (1 - 2 * (x2 + z2)) * t.getScale().getY();
-        m[1][2] = 2 * (yz - xw) * t.getScale().getZ();
-
-        m[2][0] = 2 * (xz - yw)  * t.getScale().getX();
-        m[2][1] = 2 * (yz + xw) * t.getScale().getY();
-        m[2][2] = (1 - 2 * (x2 + y2)) * t.getScale().getZ();
-
-        m[0][3] = t.getPosition().getX();
-        m[1][3] = t.getPosition().getY();
-        m[2][3] = t.getPosition().getZ();
-
-        m[3][3] = 1;
-
-        return new Matrix4f(m);
-    }
-
 }
---
+++ src/main/java/com/draconisgames/celticengine/physics/math/matrices/ProjectionMatrix.java
@@ -0,0 +1,20 @@
+package com.draconisgames.celticengine.physics.math.matrices;
+
+public class ProjectionMatrix extends Matrix4f {
+
+    public ProjectionMatrix() {
+        matrix = new float[4][4];
+    }
+
+    public ProjectionMatrix(float aspectRatio, float fov, float zNear, float zFar) {
+        float t = (float) (1/Math.tan(fov/2));
+        float zm = zFar - zNear;
+        float zp = zFar + zNear;
+        matrix[0][0] = t/aspectRatio;
+        matrix[1][1] = t;
+        matrix[2][2] = -zp/zm;
+        matrix[2][3] = -(2*zFar*zNear)/zm;
+        matrix[3][2] = -1;
+    }
+
+}
